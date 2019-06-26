408e40c80e32d2b86edafa26787018618b4880c7
yoeswin <yoeswin28@gmail.com>
2019-06-26 22:57:14 +0530

    isAlphabet

---
+++ beginner/isAlpha.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class isAlpha {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            char ch = sc.next().charAt(0);
+            if (Character.isLetter(ch)) {
+                System.out.print("Alphabet");
+            } else {
+                System.out.print("No");
+            }
+        } catch (Exception e) {
+
+        }
+    }
+}
