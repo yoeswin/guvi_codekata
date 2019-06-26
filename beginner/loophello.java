edbbfdd53f4ecdb387b8db30b4e719af617c23f1
yoeswin <yoeswin28@gmail.com>
2019-06-26 23:36:52 +0530

    Create loophello.java

---
+++ beginner/loophello.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class loophello
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    try{
+		int x=sc.nextInt();
+	    	if(x>=0){
+                for(int x1=0;x1<x;x1++)
+				    System.out.println("Hello");    
+	    	}
+		}
+		catch(Exception e){
+
+		}
+	}
+}
