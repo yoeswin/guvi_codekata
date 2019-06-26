2f96b4f8c5a20b7342db0cf4eaa96fb58db308b2
yoeswin <yoeswin28@gmail.com>
2019-06-26 22:45:32 +0530

    update

---
+++ beginner/vowel.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class vowel {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            char ch = sc.next().charAt(0);
+            if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+                if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O'
+                        || ch == 'u' || ch == 'U') {
+                    System.out.print("Vowel");
+                } else {
+                    System.out.print("Consonant");
+                }
+            } else {
+                System.out.print("invalid");
+            }
+        } catch (Exception e) {
+
+        }
+    }
+}
