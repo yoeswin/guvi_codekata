3bd3d410433bc9338b740aa58d5d3fddd76a9387
yoeswin <yoeswin28@gmail.com>
2019-06-26 22:19:06 +0530

    Create sum.java

---
+++ beginner/sum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class sum
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    try{
+			int x=sc.nextInt();
+
+	 	   if(x<0){
+	    		System.out.println("Negative");    
+	    	}else{
+		        System.out.println("Positive");
+			}
+		}
+		catch(Exception e){
+
+		}
+	}
+}
