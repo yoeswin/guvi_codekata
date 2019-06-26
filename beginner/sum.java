91b3077e4f1619b214b10ec3416060ab18f9a7ed
yoeswin Selvaraj E <34303289+yoeswin@users.noreply.github.com>
2019-06-26 22:23:28 +0530

    Update sum.java

--- beginner/sum.java
+++ beginner/sum.java
@@ -4,9 +4,10 @@ public class sum
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
 	    try{
-			int x=sc.nextInt();
-
-	 	   if(x<0){
+		int x=sc.nextInt();
+	    	if(x==0){
+			System.out.println("0");    
+	    	}else if(x<0){
 	    		System.out.println("Negative");    
 	    	}else{
 		        System.out.println("Positive");
