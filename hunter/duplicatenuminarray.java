22516d23919f1292fb8b530644360185a013df19
yoeswin <yoeswin28@gmail.com>
2019-06-27 00:30:24 +0530

    Create duplicatenuminarray.java

---
+++ hunter/duplicatenuminarray.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class duplicatenuminarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int x = sc.nextInt();
+            int[] x1 = new int[x];
+            ArrayList<Integer> duplicate = new ArrayList<>();
+            if (x >= 0) {
+                for (int temp = 0; temp < x; temp++) {
+                    x1[temp] = sc.nextInt();
+                }
+
+                for (int temp = 0; temp < x; temp++) {
+                    for (int temp1 = temp + 1; temp1 < x; temp1++)
+                        if (x1[temp] == x1[temp1]) {
+                            if (!duplicate.contains(x1[temp])){
+                                System.out.println(x1[temp]);
+                                duplicate.add(x1[temp]);
+                            }
+                        }
+                }
+            }
+        } catch (Exception e) {
+
+        } finally {
+            sc.close();
+        }
+    }
+}
