0b64e3ecd4422c33d8a2e6e1931cb83819412ae3
yoeswin <yoeswin28@gmail.com>
2019-06-27 18:34:39 +0530

    indexEqual

--- hunter/indexEqual.java
+++ hunter/indexEqual.java
@@ -1,17 +1,20 @@
 import java.util.*;
 
-public class arraySum {
+public class indexEqual {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         try {
             int x = sc.nextInt();
+            int[] array = new int[x];
             if (x > 0) {
-
                 for (int x1 = 0; x1 < x; x1++)
                     array[x1] = sc.nextInt();
 
                 int flag = 0;
                 for (int x1 = 0; x1 < x; x1++) {
+                    if (flag == 1)
+                        System.out.print(" ");
+
                     if (array[x1] == x1) {
                         System.out.print(array[x1]);
                         flag = 1;
