f7ef9aafe8566c2a6e088334516a1f5236d54489
yoeswin <yoeswin28@gmail.com>
2019-07-09 20:13:17 +0530

    reverse word

--- hunter/RevWords.java
+++ hunter/RevWords.java
@@ -2,18 +2,22 @@ import java.util.*;
 
 public class RevWords {
     public static void main(String[] a) {
-        Scanner s = new Scanner(System.in);
-        String sentence = s.nextLine();
-        String[] words = sentence.split("\\s+");
-        StringBuilder finalWord = new StringBuilder();
-        for (String word : words) {
-            StringBuilder temp = new StringBuilder(word);
-            temp.reverse();
-            finalWord.append(temp.toString()).append(" ");
+        try {
+            Scanner s = new Scanner(System.in);
+            String sentence = s.nextLine();
+            String[] words = sentence.split("\\s+");
+            StringBuilder finalWord = new StringBuilder();
+            for (String word : words) {
+                StringBuilder temp = new StringBuilder(word);
+                temp.reverse();
+                finalWord.append(temp.toString()).append(" ");
+            }
+            System.out.println(finalWord.toString());
+        } catch (Exception e) {
+
+        } finally {
+            s.close();
         }
-        System.out.println(finalWord.toString());
-        s.close();
     }
-    
 
 }
